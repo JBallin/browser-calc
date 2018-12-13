fb059d5a14b79e31f33079e94821ebefee9e6ed1
JBallin <JBallin@users.noreply.github.com>
2018-12-13 13:50:53 -0800

    Test calculateString

---
+++ test/calculateString.test.js
@@ -0,0 +1,26 @@
+const { assert } = require('chai');
+const calcStr = require('../js/calculateString');
+
+describe('calculateString', () => {
+  it('should return a number', () => {
+    assert.isNumber(calcStr('2x4'));
+  });
+  it('should multiply', () => {
+    assert.equal(calcStr('2x4'), 8);
+  });
+  it('should divide', () => {
+    assert.equal(calcStr('2÷4'), .5);
+  });
+  it('should add', () => {
+    assert.equal(calcStr('2+4'), 6);
+  });
+  it('should subtract', () => {
+    assert.equal(calcStr('2-4'), -2);
+  });
+  it('should support decimals', () => {
+    assert.equal(calcStr('.2x4'), .8);
+  });
+  it('should properly calculate a long operation', () => {
+    assert.equal(calcStr('4÷2+3x4-10÷2+7-2'), 14);
+  });
+});
