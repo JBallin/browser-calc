389328bfaa97a5955785a200c9f7f311ff7995f0
JBallin <JBallin@users.noreply.github.com>
2018-12-12 21:31:45 -0800

    Add clear and show error when using two subsequent operators

--- js/index.js
+++ js/index.js
@@ -3,10 +3,22 @@ window.onload = () => {
   const displayScreen = document.querySelector('#screen');
 
   buttons.onclick = buttonOnclick;
-  
+
   function buttonOnclick(e) {
     const value = e.target.innerHTML;
-    if (value.length === 1) {
+    if (value.length !== 1) return;
+
+    if (value === 'C') {
+      displayScreen.innerHTML = '';
+    } else if (isNaN(value)) {
+      if (value in operatorMapping) {
+        if(isNaN(displayScreen.innerHTML.slice(-1))) {
+          displayScreen.innerHTML = 'ERROR';
+        } else {
+          displayScreen.innerHTML += value;
+        }
+      }
+    } else {
       displayScreen.innerHTML += value;
     }
   };
