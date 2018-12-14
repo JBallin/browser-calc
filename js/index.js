a87143097557149207fe7adf2cacc62e4a5c6369
JBallin <JBallin@users.noreply.github.com>
2018-12-13 22:20:18 -0800

    Revert "Change disabled input back to div"
    Doesn't work properly on iOS Safari
    
    This reverts commit 139c22401107e52f04374a98460de0b478d57418.

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ window.onload = () => {
     } else if (e.key === 'Enter') {
       calculateScreen();
     } else if (e.key === 'Backspace') {
-      if (displayScreen.innerHTML.includes('E')) clearScreen();
+      if (displayScreen.value.includes('E')) clearScreen();
       else deleteLastChar();
     } else {
       handleInput(e.key);
@@ -29,7 +29,7 @@ window.onload = () => {
     else if (input === '=') calculateScreen();
     else if (isOperator(input)) addOperator(input);
     else if (isInvalidInput(input)) showError();
-    else if (wasPrevEquals || displayScreen.innerHTML === 'ERROR') overwriteScreen(input);
+    else if (wasPrevEquals || displayScreen.value === 'ERROR') overwriteScreen(input);
     else addToScreen(input);
   }
 
@@ -49,17 +49,17 @@ window.onload = () => {
   }
 
   function clearScreen() {
-    displayScreen.innerHTML = '';
+    displayScreen.value = '';
     wasPrevEquals = false;
   }
 
   function calculateScreen() {
-    if (displayScreen.innerHTML === 'ERROR') clearScreen();
-    else if (displayScreen.innerHTML) {
-      const calculation = displayScreen.innerHTML;
+    if (displayScreen.value === 'ERROR') clearScreen();
+    else if (displayScreen.value) {
+      const calculation = displayScreen.value;
       try {
-        displayScreen.innerHTML = calculateString(calculation); // eslint-disable-line no-undef
-        if (Number.isNaN(Number(displayScreen.innerHTML))) throw Error;
+        displayScreen.value = calculateString(calculation); // eslint-disable-line no-undef
+        if (Number.isNaN(Number(displayScreen.value))) throw Error;
       } catch (e) {
         showError();
       }
@@ -69,13 +69,13 @@ window.onload = () => {
 
   function addOperator(input) {
     if (wasGivenTwoOperators(input) || wasGivenOperatorFirst(input)) showError();
-    else if (displayScreen.innerHTML === 'ERROR' && input === '-') overwriteScreen('-');
-    else if (displayScreen.innerHTML !== 'ERROR') addToScreen(input);
+    else if (displayScreen.value === 'ERROR' && input === '-') overwriteScreen('-');
+    else if (displayScreen.value !== 'ERROR') addToScreen(input);
     wasPrevEquals = false;
   }
 
   function wasGivenTwoOperators(input) {
-    const displayWithoutSpaces = displayScreen.innerHTML.replace(/\s+/g, '');
+    const displayWithoutSpaces = displayScreen.value.replace(/\s+/g, '');
     const prevInput = displayWithoutSpaces.slice(-1);
     const earlierInput = displayWithoutSpaces.slice(-2, -1);
     const isFirstInput = !prevInput;
@@ -91,24 +91,24 @@ window.onload = () => {
   }
 
   function overwriteScreen(input) {
-    displayScreen.innerHTML = input;
+    displayScreen.value = input;
     wasPrevEquals = false;
   }
 
   function addToScreen(input) {
-    displayScreen.innerHTML += input;
+    displayScreen.value += input;
     wasPrevEquals = false;
   }
 
   function showError() {
-    displayScreen.innerHTML = 'ERROR';
+    displayScreen.value = 'ERROR';
   }
 
   function wasGivenOperatorFirst(input) {
-    return !displayScreen.innerHTML && isOperator(input) && input !== '-';
+    return !displayScreen.value && isOperator(input) && input !== '-';
   }
 
   function deleteLastChar() {
-    displayScreen.innerHTML = displayScreen.innerHTML.slice(0, -1);
+    displayScreen.value = displayScreen.value.slice(0, -1);
   }
 };
